535e1178369042a093b0462da7aa23982ad41dfe
Jerry Chen <jerryc05@qq.com>
2018-09-30 23:49:33 -0400

    upgrade

--- src/main/java/FlightPriceCollection.java
+++ src/main/java/FlightPriceCollection.java
@@ -1,3 +1,6 @@
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -5,77 +8,95 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 public class FlightPriceCollection {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-
         //input
         CompletableFuture<String[]> completableFuture = CompletableFuture.supplyAsync(() -> {
-            System.out.print("Input departure airport code in 3 characters: ");
-            final String departureAirport = scan.nextLine().trim().substring(0, 3);
-            System.out.print("Input   arrival airport code in 3 characters: ");
-            final String arrivalAirport = scan.nextLine().trim().substring(0, 3);
-            System.out.print("Input departure date in           yyyy-mm-dd: ");
-            final String departDate = scan.nextLine().trim().substring(0, 10);
-            System.out.print("Input return date in yyyy-mm-dd (blank for one-way trip): ");
-            String returnDate = scan.nextLine().trim();
-            return new String[]{departureAirport, arrivalAirport, departDate, returnDate};
+            JTextField departureAirportField = new JTextField(3);
+            JTextField arrivalAirportField = new JTextField(3);
+            JTextField departDateField = new JTextField(10);
+            JTextField returnDateField = new JTextField(10);
+            Object[] popupWindow = {
+                    "departure airport code:", departureAirportField,
+                    "arrival airport code:", arrivalAirportField,
+                    "departure date(YYYY-MM-DD)", departDateField,
+                    "return date(YYYY-MM-DD)", returnDateField
+            };
+            if (JOptionPane.showConfirmDialog(null, popupWindow,
+                    "Flight Search", JOptionPane.OK_CANCEL_OPTION) != JOptionPane.OK_OPTION)
+                return new String[]{};
+            else {
+                if (departureAirportField.getText().equals(""))
+                    departureAirportField.setText("foc");
+                if (arrivalAirportField.getText().equals(""))
+                    departureAirportField.setText("sha");
+                if (departDateField.getText().equals(""))
+                    departDateField.setText("2018-11-20");
+                return new String[]{departureAirportField.getText(), arrivalAirportField.getText(),
+                        departDateField.getText(), returnDateField.getText().trim()};
+            }
         });
+
 
         //init
         final long START_TIME = System.currentTimeMillis();
-        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
-        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
+//        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+//        Logger.getLogger("com.gargoylesoftware").setLevel(Level.OFF);
         System.setProperty("webdriver.chrome.driver", "D:/Download/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.setBinary("C:/Program Files (x86)/Google/Chrome Dev/Application/chrome.exe");
         options.setProxy(null);
         options.addArguments("--headless");
+//        options.addArguments("--silent");
         final ChromeDriver driver = new ChromeDriver(options);
 //        final HtmlUnitDriver driver = new HtmlUnitDriver(true);
 //        final WebClient webClient = new WebClient();
 //        HtmlPage htmlPage = null;
 //        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        System.out.println("Boot time: " + (System.currentTimeMillis() - START_TIME) / 1000d);
+
 
         //process
-        System.out.println("\n\t====== Processing, please wait... ======\n");
-        String departureAirport = "", arrivalAirport = "", departDate = "", returnDate = "";
+        String departureAirportCode = "", arrivalAirportCode = "", departDate = "", returnDate = "";
         try {
-            departureAirport = completableFuture.get()[0];
-            arrivalAirport = completableFuture.get()[1];
-            departDate = completableFuture.get()[3];
-            returnDate = completableFuture.get()[4];
+            departureAirportCode = completableFuture.get()[0];
+            arrivalAirportCode = completableFuture.get()[1];
+            departDate = completableFuture.get()[2];
+            returnDate = completableFuture.get()[3];
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("\n\t====== Processing, please wait... ======\n");
         if (returnDate.equals(""))
             driver.get(
 //            try {
 //                htmlPage = webClient.getPage(
                     "https://flights.ctrip.com/itinerary/oneway/"
-                            + departureAirport + "-" + arrivalAirport + "?date=" + departDate);
+                            + departureAirportCode + "-" + arrivalAirportCode + "?date=" + departDate);
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
         else {
             returnDate = returnDate.substring(0, 10);
             driver.get("https://flights.ctrip.com/itinerary/roundtrip/"
-                    + departureAirport + "-" + arrivalAirport + "?date=" + departDate + "%2" + returnDate);//,
+                    + departureAirportCode + "-" + arrivalAirportCode + "?date=" + departDate + "%2" + returnDate);//,
         }
         ((JavascriptExecutor) driver).executeScript("window.scrollTo(0, document.body.scrollHeight)");
 //        ((JavascriptExecutor) webClient).executeScript("window.scrollTo(0, document.body.scrollHeight)");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 //        webClient.waitForBackgroundJavaScript(10 * 1000);
-        System.out.println("\nBoot time: " + (System.currentTimeMillis() - START_TIME) / 1000d);
+
 
         //output
         System.out.println("\t====== Showing at most 100 flights sorted by price ======\n");
@@ -83,8 +104,14 @@ public class FlightPriceCollection {
 //        System.out.println(Objects.requireNonNull(htmlPage).getTitleText() + "\n");
         ArrayList<Flight> flights = new ArrayList<>();
         String prefix, suffix4Price = " > div.inb.price.child_price.lowest_price",
-                airlineFlightNumber, model, startTime, endTime, departureAp, arrivalAp, accuracy, discountRate, price;
+                airlineFlightNumber, model, startTime, endTime, departureAirport, arrivalAirport, accuracy, discountRate, price;
         int divStart = 1;
+        XSSFWorkbook wb = new XSSFWorkbook();        //创建一个工作簿 即excel文件,再在该文件中创建一个sheet
+        XSSFSheet sheet = wb.createSheet((departureAirportCode + "->" + arrivalAirportCode) + "@" + departDate);
+        XSSFRow row = sheet.createRow(0);
+        row.createCell(0).setCellValue(driver.getTitle());
+
+
         for (int i = 1; i <= 100; i++) {
             prefix = "#base_bd > div.base_main > div.searchresult_content > div:nth-child(2) > div:nth-child("
                     + divStart + ") > div:nth-child(1) > div:nth-child(" + i + ") > div";
@@ -111,8 +138,8 @@ public class FlightPriceCollection {
             model = driver.findElementByCssSelector(prefix + " > div.inb.logo > div:nth-child(2)").getText();
             startTime = driver.findElementByCssSelector(prefix + " > div.inb.right > div.time_box > strong").getText();
             endTime = driver.findElementByCssSelector(prefix + " > div.inb.left > div.time_box > strong").getText();
-            departureAp = driver.findElementByCssSelector(prefix + " > div.inb.right > div.airport").getText();
-            arrivalAp = driver.findElementByCssSelector(prefix + " > div.inb.left > div.airport").getText();
+            departureAirport = driver.findElementByCssSelector(prefix + " > div.inb.right > div.airport").getText();
+            arrivalAirport = driver.findElementByCssSelector(prefix + " > div.inb.left > div.airport").getText();
             accuracy = driver.findElementByCssSelector(prefix + " > div.inb.service > div > div > div").getText();
             if (accuracy.equals("-"))
                 accuracy = "N/A";
@@ -125,12 +152,34 @@ public class FlightPriceCollection {
                 price = driver.findElementByCssSelector(prefix + suffix4Price + " >  div > span").getText();
             }
             Flight flight = new Flight(airlineFlightNumber, model, startTime, endTime,
-                    departureAp, arrivalAp, accuracy, discountRate, price, "Ctrip");
+                    departureAirport, arrivalAirport, accuracy, discountRate, price, "Ctrip");
             flights.add(flight);
 //            System.out.println("\tfinal " + i + " = " + (System.currentTimeMillis() - current) / 1000d);
             System.out.println(flight);
+            row = sheet.createRow(i);
+            row.createCell(0).setCellValue(airlineFlightNumber);
+            row.createCell(1).setCellValue(model);
+            row.createCell(2).setCellValue(startTime);
+            row.createCell(3).setCellValue(endTime);
+            row.createCell(4).setCellValue(departureAirport);
+            row.createCell(5).setCellValue(arrivalAirport);
+            row.createCell(6).setCellValue(accuracy);
+            row.createCell(7).setCellValue(discountRate);
+            row.createCell(8).setCellValue(price);
+            row.createCell(9).setCellValue("Ctrip.com");
+            for (int j = 0; j <= 9; j++) {
+                sheet.autoSizeColumn(j);
+            }
+        }
+        try {
+            FileOutputStream out = new FileOutputStream("D:/FlightPrices.xlsx");
+            wb.write(out);
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println("\nTotal exec time: " + (System.currentTimeMillis() - START_TIME) / 1000d);
+        driver.quit();
     }
     //    driver.findElement(By.linkText("查看365天低价")).click();
 
