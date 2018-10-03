cd6839bca10fe2605c84b5c11c8e7023c8d759dd
Jerry Chen <jerryc05@qq.com>
2018-10-03 02:07:30 -0400

    https working decoding working

--- src/main/java/CrawlCtripByJson.java
+++ src/main/java/CrawlCtripByJson.java
@@ -1,11 +1,17 @@
+import org.apache.commons.compress.compressors.brotli.BrotliCompressorInputStream;
+
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.DeflaterInputStream;
+import java.util.zip.GZIPInputStream;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
@@ -22,7 +28,7 @@ class CrawlCtripByJson {
 
         ByteArrayOutputStream byteArrayOutputStream = null;
         BufferedInputStream bufferedInputStream = null;
-        String result = null;
+        String result;
         HttpsURLConnection httpsURLConnection = null;
         URL url;
 
@@ -36,12 +42,24 @@ class CrawlCtripByJson {
             SSLContext sslContext = SSLContext.getInstance("SSL");
             TrustManager[] trustManagers = {new MyX509TrustManager()};
             sslContext.init(null, trustManagers, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
 
             httpsURLConnection = (HttpsURLConnection) url.openConnection();
-            httpsURLConnection.setSSLSocketFactory(sslContext.getSocketFactory());
-            httpsURLConnection.setHostnameVerifier((arg0, arg1) -> true);
+            httpsURLConnection.setRequestMethod("GET");
+            httpsURLConnection.setInstanceFollowRedirects(true);
             httpsURLConnection.setConnectTimeout(10 * 1000);
             httpsURLConnection.setReadTimeout(10 * 1000);
+            httpsURLConnection.setDoOutput(true);
+            httpsURLConnection.setDoInput(true);
+            httpsURLConnection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+            httpsURLConnection.setRequestProperty("Accept-Encoding", "gzip, deflate, br");
+            httpsURLConnection.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7");
+            httpsURLConnection.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            httpsURLConnection.setRequestProperty("DNT", "1");
+            httpsURLConnection.setRequestProperty("Upgrade-Insecure-Requests", "1");
+            httpsURLConnection.setRequestProperty("Connection", "keep-alive");
+            httpsURLConnection.setRequestProperty("Host", "flights.ctrip.com");
+            httpsURLConnection.setHostnameVerifier((hostname, session) -> true);
             if (cookieMap != null) {
                 StringBuilder parseCookie = new StringBuilder();
                 for (String keyOfACookie : cookieMap.keySet())
@@ -51,25 +69,25 @@ class CrawlCtripByJson {
                             .append(";");
                 httpsURLConnection.setRequestProperty("Cookie", parseCookie.toString());
             }
-            httpsURLConnection.setRequestProperty("connection", "Keep-Alive");
-            httpsURLConnection.setRequestProperty("user-agent",
-                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-//            httpsURLConnection.setRequestProperty(":authority","flights.ctrip.com");
-//            httpsURLConnection.setRequestProperty(":method","GET");
-//            httpsURLConnection.setRequestProperty(":path","/itinerary/oneway/foc-sha?date=2018-12-21");
-//            httpsURLConnection.setRequestProperty(":scheme","https");
-//            httpsURLConnection.setRequestProperty("dnt", "1");
-//            httpsURLConnection.setRequestProperty("upgrade-insecure-requests", "1");
-//            httpsURLConnection.setRequestProperty("user-agent", "");
-//            httpsURLConnection.setRequestProperty("Charset", "utf-8");
-//            httpsURLConnection.setRequestProperty("accept", "application/json");
-//            httpsURLConnection.setRequestProperty("Content-Type", "application/json");
             httpsURLConnection.connect();
 
             if (httpsURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 if (httpsURLConnection.getContentLength() > 0) {
-                    bufferedInputStream = new
-                            BufferedInputStream(httpsURLConnection.getInputStream());
+                    InputStream inputStream;
+                    switch (httpsURLConnection.getContentEncoding()) {
+                        case "gzip":
+                            inputStream = new GZIPInputStream(httpsURLConnection.getInputStream());
+                            break;
+                        case "deflate":
+                            inputStream = new DeflaterInputStream(httpsURLConnection.getInputStream());
+                            break;
+                        case "br":
+                            inputStream = new BrotliCompressorInputStream(httpsURLConnection.getInputStream());
+                            break;
+                        default:
+                            inputStream = httpsURLConnection.getInputStream();
+                    }
+                    bufferedInputStream = new BufferedInputStream(inputStream);
                     byteArrayOutputStream = new ByteArrayOutputStream();
                     byte[] b = new byte[httpsURLConnection.getContentLength()];
                     int length;
@@ -77,6 +95,18 @@ class CrawlCtripByJson {
                         byteArrayOutputStream.write(b, 0, length);
                     }
                     result = byteArrayOutputStream.toString(StandardCharsets.UTF_8);
+                    System.out.println(result);
+
+
+                    InputStream input = httpsURLConnection.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(input, "gb2312");
+                    StringBuilder sb1 = new StringBuilder();
+                    int ss;
+                    while ((ss = reader.read()) != -1) {
+                        sb1.append((char) ss);
+                        result = sb1.toString();
+                    }
+                    System.out.println(result);
                 }
 
                 //get cookie
@@ -115,7 +145,6 @@ class CrawlCtripByJson {
                 e.printStackTrace();
             }
         }
-        System.out.println(result);
 
         return true;
     }
